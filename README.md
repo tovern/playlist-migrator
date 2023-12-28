b9a82650b50914d4092873b4917b81c634be46b9
Tom Vernon <33895106+tovern@users.noreply.github.com>
2023-12-28 17:16:22 +0000

    fix: remove debugging

--- README.md
+++ README.md
@@ -12,5 +12,22 @@ main.py [-h] {spotify,youtube} {import,export}
 
 ## Provider Integration
 
-YTMusic
-Spotify
+### YTMusic
+
+Simply run:
+```bash
+ytmusicapi oauth
+```
+and follow the instructions. This will create a file oauth.json in the current directory.
+
+### Spotify
+
+Add a new app to the [Spotify API](https://developer.spotify.com/dashboard/). Pay attention to the SPOTIPY_REDIRECT_URI, this does NOT need to be a valid resolvable URI, but it must match the environmental variable that you set later.
+
+Export the following variables:
+```bash
+export SPOTIPY_CLIENT_ID=XXX
+export SPOTIPY_CLIENT_SECRET=XXX
+export SPOTIPY_REDIRECT_URI=http://localhost:7777/callback
+```
+
--- provider.py
+++ provider.py
@@ -13,19 +13,18 @@ class Provider:
 
     def export_tracks(self):
         """Writes tracks to a JSON file"""
-        with open('output.json', 'w') as file:
+        with open('playlist.json', 'w') as file:
             file.write(json.dumps(self.tracks))  # use `json.loads` to do the reverse
 
     def import_tracks(self):
         """Loads tracks from a JSON file"""
-        with open('output.json', 'r') as file:
+        with open('playlist.json', 'r') as file:
             self.tracks = (json.load(file))
 
     def spotify_init(self):
         """Initiate connection to Spotify API"""
         scope = "user-library-read user-library-modify playlist-modify-private"
         self.provider = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-        print("sp")
 
     def spotify_fetch_tracks(self):
         """Gets all tracks from a users 'saved tracks/liked songs' playlist in Spotify"""
@@ -57,7 +56,6 @@ class Provider:
     def youtube_init(self):
         """Initiate connection to YouTube API"""
         self.provider = YTMusic("oauth.json")
-        print("yt")
 
     def youtube_fetch_tracks(self):
         """Gets all tracks from a users 'Liked Music' playlist in YouTube Music"""
@@ -77,6 +75,6 @@ class Provider:
             # Search for the top matching track ID
             results = self.provider.search(track["searchString"], "songs")
             track["id"] = results[0]["videoId"]
-            # Add to playlist. Replace playlist ID with LM when live
-            self.provider.add_playlist_items("VLPLKRob01e0UOV9l-ue_fmoLJK8kGcZikEm", [track["id"]])
+            # Add to playlist.
+            self.provider.add_playlist_items("LM", [track["id"]])
         print(f"Added {len(self.tracks)} tracks to YouTube playlist")
